b5e465386bf0df9846be9bd6771edb9a20a2c329
Ryan <ryandeblok167@gmail.com>
2026-02-12 12:11:44 +0100

    Models
    Start van de models User, Team, Role

---
+++ ToDoList/model/Team.cs
@@ -0,0 +1,8 @@
+public class Team
+{
+    public int ID { get; set; }
+
+    public string TeamName { get; set; }
+
+    public User user { get; set; }
+}
---
+++ ToDoList/model/User.cs
@@ -0,0 +1,14 @@
+public class User
+{
+    public int ID { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Age { get; set; }
+
+    public Role role {get; set; }
+
+    public Task task {get; set; }
+}
