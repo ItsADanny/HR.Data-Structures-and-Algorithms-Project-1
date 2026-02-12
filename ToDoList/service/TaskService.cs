09dd117d91b76d68bd502711716951f02e3497ef
hengyi2002 <hengyi2002@hotmail.com>
2026-02-12 13:16:30 +0100

    + crud

---
+++ ToDoList/service/TaskService.cs
@@ -0,0 +1,75 @@
+public class TaskService : ITaskService
+{
+    public TaskItem[] TasksList = new TaskItem[] {};
+    int LastIndex = 0;
+    public IEnumerable<TaskItem> GetAllTasks()
+    {
+        return TasksList; 
+    }
+
+    public void AddTask(TaskItem task)
+    {
+        if (LastIndex < TasksList.Length - 1)
+        {
+            LastIndex++;
+            TasksList[LastIndex] = task;
+            return; 
+        } 
+        addslots();
+        LastIndex++;
+        TasksList[LastIndex] = task;
+        return;
+    }
+    
+    public void UpdateTask(TaskItem task)
+    {
+        int index = findTaskById(task.ID);
+        TasksList[index] = task; return;
+    }
+
+    public int findTaskById(int id)
+    {
+        for (int i = 0; i < TasksList.Length; i++)
+        {
+            if (TasksList[i].ID == id)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void DeleteTask(int id)
+    {
+        int index = findTaskById(id);
+        if(index != -1)
+        {
+            shiftToleft(index);
+            LastIndex--;
+        }
+        return;
+    }
+
+    public void shiftToleft( int index) 
+    { 
+        for (int x = index; x < LastIndex; x++) 
+        {
+            TasksList[x] = TasksList[x + 1];
+        }
+    }
+
+    public void addslots()
+    {
+        TaskItem[] newlist = new TaskItem[TasksList.Length + 10];
+        for (int i = 0; i < TasksList.Length; i++)
+        {
+            newlist[i] = TasksList[i];
+        }
+        TasksList = newlist;
+    }
+
+    public void ToggleTaskCompletion(int id)
+    {
+        // Implementation here
+        return ;
+    }
+}
